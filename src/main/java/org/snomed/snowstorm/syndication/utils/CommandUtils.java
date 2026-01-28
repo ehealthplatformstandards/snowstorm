8fb83a14365d6154159bd31e6a3a4fbf3951cac4
Aime Kodjane <aime.kodjane@ehealth.fgov.be>
2026-01-28 14:56:08 +0100

    reverted the breaking change

--- src/main/java/org/snomed/snowstorm/syndication/utils/CommandUtils.java
+++ src/main/java/org/snomed/snowstorm/syndication/utils/CommandUtils.java
@@ -18,7 +18,7 @@ public class CommandUtils {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    logger.info("[STDOUT] {}", line);
+                    logger.info(line);
                 }
             } catch (IOException e) {
                 logger.error("Error reading stdout", e);
@@ -27,8 +27,11 @@ public class CommandUtils {
 
         Thread stderrReader = new Thread(() -> {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
-                terminateProcessAfterFirstStacktrace(process, reader);
-            } catch (IOException | InterruptedException e) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    logger.error(line);
+                }
+            } catch (IOException e) {
                 logger.error("Error reading stderr", e);
             }
         });
@@ -62,19 +65,5 @@ public class CommandUtils {
         }
 
         return version;
-    }
-
-    private static void terminateProcessAfterFirstStacktrace(Process process, BufferedReader reader) throws IOException, InterruptedException {
-        String line;
-        if ((line = reader.readLine()) != null) {
-            logger.error("[STDERR] {}", line);
-            Thread.sleep(2000);
-            while (reader.ready()) {
-                logger.error("[STDERR] {}", reader.readLine());
-                Thread.sleep(200);
-            }
-            process.destroyForcibly();
-            logger.error("Process terminated due to error output");
-        }
     }
 }
